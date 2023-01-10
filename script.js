63a2a963648956cf8466013130346b8e7f55b8f2
francostroff <116038334+francostroff@users.noreply.github.com>
2023-01-10 23:16:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'
+];
 
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a','b','c','d','e','f','g','h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A', 'B', 'C', 'D','E','F', 'G', 'H','I', 'J','K','L','M','N','O','P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+];
 // Function to prompt user for password options
-    
-let lenght = prompt("How many characters would you like your password to contain?");
-let special = prompt("Include special character?");
-if (lenght>=10) {
+let lenght = prompt("How many characters would you like in your password?");
+if (lenght>64) {
+  alert("max lenght 64 characters")
+}
+let special = prompt("Do you need any special characters - y or n yes or no?");
+if (special === "y"){
+  alert(`OK`);
+}
+let numbers = prompt("Would you like to include NUMBERS?");
+{
 alert("Welcome!");
 }
 
 
 // Function to generate password with user input
 function generatePassword() {
-const lenght= 16;
-const characters = "abcdefghijlmnopqrtuvxyzABCDEFGHIJLMNOPQRTUVXYZ0123456789!@£#%^&*()/?";
+const lenght= 24;
+const allCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','a','b','c','d','e','f','g','h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+  'A', 'B', 'C', 'D','E','F', 'G', 'H','I', 'J','K','L','M','N','O','P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
 let password = "";
 for (let i=0; i < lenght; i++){
-  const character = characters[Math.floor(Math.random()*characters.length)];
+  const character = allCharacters[Math.floor(Math.random()*allCharacters.length)];
 password += character;
 }
+console.log (password);
 return password;
-
 }
 
 // Get references to the #generate element
